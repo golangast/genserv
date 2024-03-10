cef1ffc1ca425f810aeec789bdd68264f5c3d179
zach endrulat <golangast@gmail.com>
2024-03-10 09:01:52 -0500

    udpated db

--- internal/dbsql/dbconn/dbconn.go
+++ internal/dbsql/dbconn/dbconn.go
@@ -3,12 +3,15 @@ package dbconn
 import (
 	"fmt"
 
+	"github.com/golangast/genserv/internal/rand"
 	"github.com/rqlite/gorqlite"
 	_ "modernc.org/sqlite"
 )
 
 func DbConnection() (*gorqlite.Connection, error) {
-	conn, err := gorqlite.Open("http://bill:secret1@localhost:4001/")
+	rr := rand.Rander()
+	rrr := rand.Rander()
+	conn, err := gorqlite.Open("http://" + rrr + ":" + rr + "@localhost:4001/")
 	return conn, err
 } //end of connect
 func ErrorCheck(err error) {
