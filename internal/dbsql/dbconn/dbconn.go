27be0d3d7be7271f7874e061d06378fff54ef1c3
golangast <zendrulat@gmail.com>
2024-03-11 20:08:11 -0500

    updated database dbconn

--- internal/dbsql/dbconn/dbconn.go
+++ internal/dbsql/dbconn/dbconn.go
@@ -3,15 +3,13 @@ package dbconn
 import (
 	"fmt"
 
-	"github.com/golangast/genserv/internal/rand"
 	"github.com/rqlite/gorqlite"
 	_ "modernc.org/sqlite"
 )
 
 func DbConnection() (*gorqlite.Connection, error) {
-	rr := rand.Rander()
-	rrr := rand.Rander()
-	conn, err := gorqlite.Open("http://" + rrr + ":" + rr + "@localhost:4001/")
+
+	conn, err := gorqlite.Open("http://localhost:4001/")
 	return conn, err
 } //end of connect
 func ErrorCheck(err error) {
